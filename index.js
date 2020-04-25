9e8dc8635e99700510112ec871856c90f0b8bfcb
Gagan <gaganjot.singh.randhawa@sap.com>
2020-04-25 23:35:11 +0530

    fsdfgfdsdfg

---
+++ index.js
@@ -0,0 +1,7 @@
+var arrayExample = [1, 3, 4, 5];
+var example1 = 35;
+var example2;
+example2 = 35;
+var example3 = "Hello world";
+var example4 = undefined;
+var example5 = null;
---
+++ index.ts
@@ -0,0 +1,12 @@
+const arrayExample = [1, 3, 4, 5];
+
+let example1: boolean | number = 35;
+
+let example2: number;
+example2 = 35;
+
+let example3 = "Hello world";
+
+let example4: undefined = undefined;
+
+let example5: null = null;
